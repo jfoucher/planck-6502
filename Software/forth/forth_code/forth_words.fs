530989db7b1f9a7784e9f109b6ada3b508f79223
Jonathan Foucher <jfoucher@gmail.com>
2021-09-22 23:21:39 +0200

    remove tali forth verbose printing on boot

--- Software/forth/forth_code/forth_words.fs
+++ Software/forth/forth_code/forth_words.fs
@@ -19,10 +19,12 @@
 \ generated at the end of the boot process and signal that the other
 \ high-level definitions worked (or at least didn't crash)
 
-        cr .( Tali Forth 2 for the 65c02)
-        cr .( Version 1.0  24. Jan 2020 )
-        cr .( Copyright 2014-2020 Scot W. Stevenson)
-        cr .( Tali Forth 2 comes with absolutely NO WARRANTY)
-        cr .( Type 'bye' to exit) cr
+\ cr .( Tali Forth 2 for the 65c02)
+\ cr .( Version 1.0  24. Jan 2020 )
+\ cr .( Copyright 2014-2020 Scot W. Stevenson)
+\ cr .( Tali Forth 2 comes with absolutely NO WARRANTY)
+\ cr .( Type 'bye' to exit) cr
+
+cr .( Welcome to Planck 6502 ) cr
 
 \ END
